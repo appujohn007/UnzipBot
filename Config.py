fa942429c41019a9f798917dfe9d242785cf64bc
Appu John <161853537+appujohn007@users.noreply.github.com>
2024-06-15 18:45:42 +0530

    Update Config.py

--- Config.py
+++ Config.py
@@ -2,11 +2,11 @@ import os
 
 
 class Config:
-    API_ID = int(os.environ.get("API_ID", 0))  # Change 12345 to your API_ID
-    API_HASH = os.environ.get("API_HASH", None)  # Change None to your API_HASH
-    BOT_TOKEN = os.environ.get("BOT_TOKEN", None)  # Change None to your BOT_TOKEN
-    OWNER_ID = int(os.environ.get("OWNER_ID", 0))  # Change 0 to your OWNER_ID
-    OWNER_NAME = os.environ.get("OWNER_NAME", None)  # Change None to your OWNER_NAME
+    API_ID = int(os.environ.get("API_ID", 10471716))  # Change 12345 to your API_ID
+    API_HASH = os.environ.get("API_HASH", f8a1b21a13af154596e2ff5bed164860)  # Change None to your API_HASH
+    BOT_TOKEN = os.environ.get("BOT_TOKEN", 6916875347:AAEVxR4cO_sIBB6V57ANA92pHKxzw9G3yX0)  # Change None to your BOT_TOKEN
+    OWNER_ID = int(os.environ.get("OWNER_ID", 6883997969))  # Change 0 to your OWNER_ID
+    OWNER_NAME = os.environ.get("OWNER_NAME", "Appus")  # Change None to your OWNER_NAME
 
     # For Local Deploys edit above 5 lines.
     # Put your API_ID and OWNER_ID (without comma) and API_HASH,BOT_TOKEN n OWNER_NAME (with commas) below.
